a1115be1097e327b2b6581f0685e753484c8e993
Maxwell Zimon <max@zimon.co>
2019-08-01 15:57:44 -0500

    genericize ShimmedMethod into ShimmedMember, ShimmedMethod, ShimmedConstructor

---
+++ Shimmy/Data/ShimmedConstructor.cs
@@ -0,0 +1,67 @@
+﻿using Shimmy.Helpers;
+using System;
+using System.Linq;
+using System.Linq.Expressions;
+using System.Reflection;
+using System.Reflection.Emit;
+
+namespace Shimmy.Data
+{
+    internal class ShimmedConstructor<T> : ShimmedMember<ConstructorInfo>
+    {
+        public ConstructorInfo Constructor => Member;
+
+        public T ReturnValue { get; set; }
+
+        protected override ParameterExpression[] GenerateExpressionParameters()
+        {
+            var parameters = Constructor.GetParameters();
+            return GetExpressionParametersFromParameterInfo(parameters);
+        }
+
+        protected override Expression<Action> GenerateCallExpression()
+        {
+            return Expression.Lambda<Action>(Expression.New(Constructor, ExpressionParameters));
+        }
+
+        // todo: constructors for value types
+
+        protected override Delegate GetShimAction()
+        {
+            if (!ExpressionParameters.Any())
+                return (Func<T>)(() => LogAndReturnOrCreate());
+
+            return GenerateDynamicShim();
+        }
+
+        private T LogAndReturnOrCreate()
+        {
+            AddCallResult();
+
+            // todo: improve this
+            if (ReturnValue != null)
+                return ReturnValue;
+
+            // todo: objects with only constructors that are parameterized
+            return Activator.CreateInstance<T>();
+        }
+
+        private Delegate GenerateDynamicShim()
+        {
+            var expressionParamsArray = ExpressionParameters.Select(p => p.Type).ToArray();
+
+            var dynamicMethod = new DynamicMethod("shimmy_ctor_" + Constructor.Name,
+                MethodAttributes.Public | MethodAttributes.Static,
+                CallingConventions.Standard,
+                Constructor.DeclaringType,
+                expressionParamsArray,
+                Constructor.DeclaringType,
+                false);
+
+            return ShimmedMemberHelper.GenerateDynamicShim(dynamicMethod,
+                LibraryReferenceGuid,
+                expressionParamsArray,
+                Constructor.DeclaringType);
+        }
+    }
+}
--- Shimmy/Data/ShimmedMethod.cs
+++ Shimmy/Data/ShimmedMethod.cs
@@ -9,47 +9,13 @@ using System.Reflection.Emit;
 
 namespace Shimmy.Data
 {
-    internal class ShimmedMethod
+    internal class ShimmedMethod : ShimmedMember<MethodInfo>
     {
-        public const int MaximumPoseParameters = 10;
         public const string InvalidReturnTypeError = "Cannot set return value of type {0} on a method with return value of type {1}.";
-        public const string CannotSetReturnTypeOnVoidMethodError = "Cannot set return value on of a method with return type void.";
 
-        private Guid _libraryReferenceGuid;
-        protected ParameterExpression[] _expressionParameters;
-        protected Type DeclaringType;
-
-
-        public MethodInfo Method { get; private set; }
-
-        public List<ShimmedMethodCall> CallResults { get; private set; }
-
-        public Shim Shim { get; private set; }
-
-        public ShimmedMethod(MethodInfo method)
-        {
-            Init(method);
-        }
-
-        protected ShimmedMethod() { }
-
-        protected void Init(MethodInfo method)
-        {
-            Method = method ?? throw new ArgumentNullException(nameof(method));
-            _expressionParameters = GenerateExpressionParameters();
-            _libraryReferenceGuid = ShimmedMethodLibrary.Add(this);
-            DeclaringType = method.DeclaringType;
-
-            Shim = GenerateShim();
-            CallResults = new List<ShimmedMethodCall>();
-        }
-
-        protected virtual Shim GenerateShim()
-        {
-            return Shim.Replace(GenerateCallExpression()).With(GetShimAction());
-        }
-
-        protected Expression<Action> GenerateCallExpression()
+        public MethodInfo Method => Member;
+        
+        protected override Expression<Action> GenerateCallExpression()
         {
             Expression poseIsACall = null;
             if (!Method.IsStatic)
@@ -57,16 +23,24 @@ namespace Shimmy.Data
                 var poseIsAMethod = typeof(Pose.Is).GetMethod("A").MakeGenericMethod(new[] { DeclaringType });
                 poseIsACall = Expression.Call(null, poseIsAMethod);
             }
-            return Expression.Lambda<Action>(Expression.Call(poseIsACall, Method, _expressionParameters));
+            return Expression.Lambda<Action>(Expression.Call(poseIsACall, Method, ExpressionParameters));
         }
 
-        private Delegate GetShimAction()
+        public ShimmedMethod(MethodInfo method) : base(method)
+        {
+        }
+
+        protected ShimmedMethod()
+        {
+        }
+       
+        protected override Delegate GetShimAction()
         {
             // if it's not necessary, don't build a dynamic method
             // this shortcut bypasses the ShimmedMethodLibrary
             // and the overhead of a dynamicmethod
             // while still logging the call
-            if (!_expressionParameters.Any() && Method.IsStatic)
+            if (!ExpressionParameters.Any() && Method.IsStatic)
                 return (Action)(() => AddCallResult());
 
             return GenerateDynamicShim();
@@ -74,7 +48,7 @@ namespace Shimmy.Data
 
         protected Delegate GenerateDynamicShim(Type returnType = null)
         {
-            var expressionParamsArray = _expressionParameters.Select(p => p.Type).ToArray();
+            var expressionParamsArray = ExpressionParameters.Select(p => p.Type).ToArray();
 
             Type[] paramTypesArray;
             if (Method.IsStatic)
@@ -97,87 +71,16 @@ namespace Shimmy.Data
                 DeclaringType,
                 false);
 
-            var ilGenerator = dynamicMethod.GetILGenerator();
-            var returnLabel = ilGenerator.DefineLabel();
+            return ShimmedMemberHelper.GenerateDynamicShim(dynamicMethod, 
+                LibraryReferenceGuid, 
+                paramTypesArray, 
+                returnType);
+       }
 
-            var arrayLocal = ilGenerator.DeclareLocal(typeof(object[]));
-
-            // mark the current shimmed method as the one to send call results
-            // todo: for now, guid is loaded as a string for convenience of not having to pointerize
-            ilGenerator.Emit(OpCodes.Ldstr, _libraryReferenceGuid.ToString());
-            ilGenerator.EmitCall(OpCodes.Call, typeof(ShimmedMethodLibrary).GetMethod("SetRunningMethod"), null);
-
-            // create a new object array of necessary length
-            ilGenerator.Emit(OpCodes.Ldc_I4, paramTypesArray.Length);
-            ilGenerator.Emit(OpCodes.Newarr, typeof(object));
-            ilGenerator.Emit(OpCodes.Stloc, arrayLocal);
-
-            // load each parameter into the object array
-            for (int i = 0; i < paramTypesArray.Length; i++)
-            {
-                // set current array index
-                ilGenerator.Emit(OpCodes.Ldloc, arrayLocal);
-                ilGenerator.Emit(OpCodes.Ldc_I4, i);
-
-                // load the parameter
-                ilGenerator.Emit(OpCodes.Ldarg, i);
-
-                // if this is a value type, box it
-                if (paramTypesArray[i].IsValueType)
-                    ilGenerator.Emit(OpCodes.Box, paramTypesArray[i]);
-
-                // save the element into the array
-                ilGenerator.Emit(OpCodes.Stelem_Ref);
-            }
-
-            // call the method which will save these parameters
-            ilGenerator.Emit(OpCodes.Ldloc, arrayLocal);
-            ilGenerator.EmitCall(OpCodes.Call, typeof(ShimmedMethodLibrary).GetMethod("AddCallResultToShim"), null);
-
-            // return - with default return value if necessary
-            // provided via a call so the stack will accomodate it
-            if (returnType == null)
-            {
-                ilGenerator.EmitCall(OpCodes.Call, typeof(ShimmedMethodLibrary).GetMethod("ClearRunningMethod"), null);
-            }
-            else
-            {
-                var method = typeof(ShimmedMethodLibrary).GetMethod("GetReturnValueAndClearRunningMethod").MakeGenericMethod(new Type[] { returnType });
-                ilGenerator.EmitCall(OpCodes.Call, method, null);
-            }
-
-            ilGenerator.MarkLabel(returnLabel);
-            ilGenerator.Emit(OpCodes.Ret);
-
-            return dynamicMethod.CreateDelegate(DelegateTypeHelper.GetTypeForDelegate(paramTypesArray, returnType));
-        }
-
-        protected ParameterExpression[] GenerateExpressionParameters()
+        protected override ParameterExpression[] GenerateExpressionParameters()
         {
             var parameters = Method.GetParameters();
-
-            if (parameters.Length > MaximumPoseParameters)
-                throw new ArgumentException("Method " + Method.Name + " has " + parameters.Length
-                    + " parameters. Pose only supports methods with " + MaximumPoseParameters + " parameters or fewer.");
-
-            var expressionParameters = new ParameterExpression[parameters.Length];
-
-            for (var i = 0; i < parameters.Length; i++)
-            {
-                expressionParameters[i] =
-                    Expression.Parameter(parameters[i].ParameterType, parameters[i].Name);
-            }
-
-            return expressionParameters;
-        }
-
-        protected void AddCallResult() => AddCallResultWithParams(new object[] { });
-
-        protected void AddCallResultWithParams(params object[] parameters) => CallResults.Add(new ShimmedMethodCall(parameters, Method));
-
-        public virtual void SetReturnValue(object value)
-        {
-            throw new InvalidOperationException(CannotSetReturnTypeOnVoidMethodError);
+            return GetExpressionParametersFromParameterInfo(parameters);
         }
     }
 
@@ -207,7 +110,7 @@ namespace Shimmy.Data
 
         private Delegate GetShimActionWithReturn()
         {
-            if(!_expressionParameters.Any() && Method.IsStatic && !ReturnValueIsDefaultForType)
+            if(!ExpressionParameters.Any() && Method.IsStatic && !ReturnValueIsDefaultForType)
                 return (Func<T>)(() => LogAndReturn());
 
             return GenerateDynamicShim(typeof(T));
